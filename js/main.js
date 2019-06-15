f7b8e6d9e18367fbc23e3393025c3b2d4a2d5eef
Katsiaryna31 <45047074+Katsiaryna31@users.noreply.github.com>
2019-06-15 16:10:53 +0300

    Личный проект: пока все дома

--- js/main.js
+++ js/main.js
@@ -1,54 +1,60 @@
 'use strict'
 
-var NUMBER = 9;
+var NUMBER = 10;
+var NUMBER_ADVERTS = 8;
 
 var getAvatar = function (k) {
   return k < NUMBER ? 'img/avatars/user' + '0' + k + '.png' : 'img/avatars/user' + k + '.png';
 };
 
-var kindPlace = ['palace', 'flat', 'house', 'bungalo'];
+var KIND_PLACE = ['palace', 'flat', 'house', 'bungalo'];
 
 var getRandomValue = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
+var getOneAdvert = function(z) {
+  var typeLive = Math.floor(Math.random() * KIND_PLACE.length);
+  var oneAdvert = {
+    author: {
+      avatar: getAvatar (z+1)
+    },
+    offer: {
+      type: KIND_PLACE[typeLive]
+    },
+    location: {
+      x: getRandomValue(1, 1200) + 'px',
+      y: getRandomValue(130, 630) + 'px'
+    }
+  }
+  return oneAdvert;
+}
+
 var getAdvertising = function () {
   var advertisement = [];
-  for (var i = 0; i < 8; i++) {
-    var typeLive = Math.floor(Math.random() * kindPlace.length);
-    var oneAdvert = {
-      author: {
-        avatar: getAvatar (i+1)
-      },
-      offer: {
-        type: kindPlace[typeLive]
-      },
-      location: {
-        x: getRandomValue(1, 1200) + 'px',
-        y: getRandomValue(130, 630) + 'px'
-      }
-    };
-    advertisement.push(oneAdvert);
+  for (var i = 0; i < NUMBER_ADVERTS; i++) {
+    var oneAd = getOneAdvert(i);
+    advertisement.push(oneAd);
   }
   return advertisement;
 };
 
 var map = document.querySelector('.map');
 map.classList.remove('map--faded');
+
 var pinList = document.querySelector('.map__pins');
 
 var pin = document.querySelector('#pin')
     .content
     .querySelector('.map__pin');
 
-var renderAdvert = function (oneAdvert) {
+var renderAdvert = function (oneAdvertisment) {
   var oneAdvertElement = pin.cloneNode(true);
 
-  oneAdvertElement.querySelector('.map__pin');
-  oneAdvertElement.style.left = oneAdvert.location.X;
-  oneAdvertElement.style.top = oneAdvert.location.Y;
-  oneAdvertElement.src = oneAdvert.author.avatar;
-  oneAdvertElement.alt = oneAdvert.offer.type;
+  oneAdvertElement.style.left = oneAdvertisment.location.x;
+  oneAdvertElement.style.top = oneAdvertisment.location.y;
+  oneAdvertElement.querySelector('img').src = oneAdvertisment.author.avatar;
+  oneAdvertElement.querySelector('img').alt = oneAdvertisment.offer.type;
 
   return oneAdvertElement;
 };
