853273fc923dbe9957f702c539e86a6c4f7d5d96
Katsiaryna31 <45047074+Katsiaryna31@users.noreply.github.com>
2019-06-26 22:14:19 +0300

    Личный проект: модуляция 2

--- js/map.js
+++ js/map.js
@@ -1,134 +1,133 @@
 'use strict';
 
-(function() {
+(function () {
 
-    var MAIN_PIN_LOCATION_X = 570;
-    var MAIN_PIN_LOCATION_Y = 375;
-    var MAIN_PIN_WIDTH = 64;
-    var MAIN_PIN_HEIGHT = 80;
-    var mainPinCenterX = MAIN_PIN_LOCATION_X + MAIN_PIN_WIDTH / 2;
-    var mainPinCenterY = MAIN_PIN_LOCATION_Y + MAIN_PIN_HEIGHT;
-    var mainPinPositionFirst = mainPinCenterX + ',' + mainPinCenterY;
-    var MAP_TOP_SIDE = 130;
-    var MAP_BOTTOM_SIDE = 630;
-    var MAP_LEFT_LIMIT = 0;
+  var MAIN_PIN_LOCATION_X = 570;
+  var MAIN_PIN_LOCATION_Y = 375;
+  var MAIN_PIN_WIDTH = 64;
+  var MAIN_PIN_HEIGHT = 80;
+  var mainPinCenterX = MAIN_PIN_LOCATION_X + MAIN_PIN_WIDTH / 2;
+  var mainPinCenterY = MAIN_PIN_LOCATION_Y + MAIN_PIN_HEIGHT;
+  var mainPinPositionFirst = mainPinCenterX + ',' + mainPinCenterY;
+  var MAP_TOP_SIDE = 130;
+  var MAP_BOTTOM_SIDE = 630;
+  var MAP_LEFT_LIMIT = 0;
 
-    var pinList = document.querySelector('.map__pins');
-    var mapSizes = pinList.getBoundingClientRect();
-    var mapRightLimit = mapSizes.width - MAIN_PIN_WIDTH;
-    var mapTopLimit = MAP_TOP_SIDE - MAIN_PIN_HEIGHT;
-    var mapBottomLimit = MAP_BOTTOM_SIDE - MAIN_PIN_HEIGHT;
+  var pinList = document.querySelector('.map__pins');
+  var mapSizes = pinList.getBoundingClientRect();
+  var mapRightLimit = mapSizes.width - MAIN_PIN_WIDTH;
+  var mapTopLimit = MAP_TOP_SIDE - MAIN_PIN_HEIGHT;
+  var mapBottomLimit = MAP_BOTTOM_SIDE - MAIN_PIN_HEIGHT;
 
-    var map = document.querySelector('.map');
-    var mainPin = document.querySelector('.map__pin--main');
+  var map = document.querySelector('.map');
+  var mainPin = document.querySelector('.map__pin--main');
 
-    var pin = document.querySelector('#pin')
-        .content
-        .querySelector('.map__pin');
+  var pin = document.querySelector('#pin')
+      .content
+      .querySelector('.map__pin');
 
-    var renderAdvert = function(oneAdvertisment) {
-        var advertElement = pin.cloneNode(true);
+  var renderAdvert = function (oneAdvertisment) {
+      var advertElement = pin.cloneNode(true);
 
-        advertElement.style.left = oneAdvertisment.location.x;
-        advertElement.style.top = oneAdvertisment.location.y;
-        advertElement.querySelector('img').src = oneAdvertisment.author.avatar;
+      advertElement.style.left = oneAdvertisment.location.x;
+      advertElement.style.top = oneAdvertisment.location.y;
+      advertElement.querySelector('img').src = oneAdvertisment.author.avatar;
 
-        return advertElement;
-    };
+      return advertElement;
+  };
 
-    var fragment = document.createDocumentFragment();
-    var advertisments = window.data.getAdvertising();
-    for (var j = 0; j < advertisments.length; j++) {
-        fragment.appendChild(renderAdvert(advertisments[j]));
-    }
-    pinList.appendChild(fragment);
+  var fragment = document.createDocumentFragment();
+  var advertisments = window.data.getAdvertising();
+  for (var j = 0; j < advertisments.length; j++) {
+      fragment.appendChild(renderAdvert(advertisments[j]));
+  }
+  pinList.appendChild(fragment);
 
-    var pinElemList = document.querySelectorAll('.map__pin');
-    for (var i = 0; i < pinElemList.length; i++) {
-        if (!pinElemList[i].classList.contains('map__pin--main')) {
-            pinElemList[i].parentNode.removeChild(pinElemList[i]);
-        }
-    }
+  var pinElemList = document.querySelectorAll('.map__pin');
+  for (var i = 0; i < pinElemList.length; i++) {
+      if (!pinElemList[i].classList.contains('map__pin--main')) {
+          pinElemList[i].parentNode.removeChild(pinElemList[i]);
+      }
+  }
 
-    mainPin.addEventListener('mouseup', function() {
-        window.form.activatePage();
-    });
+  mainPin.addEventListener('mouseup', function () {
+      window.form.activatePage();
+  });
 
-    mainPin.addEventListener('mousedown', function(evt) {
-        evt.preventDefault();
+  mainPin.addEventListener('mousedown', function (evt) {
+      evt.preventDefault();
 
-        window.form.activatePage();
+      window.form.activatePage();
 
-        var startCoords = {
-            x: evt.clientX,
-            y: evt.clientY
-        };
+      var startCoords = {
+          x: evt.clientX,
+          y: evt.clientY
+      };
 
-        var dragged = false;
+      var dragged = false;
 
+      var onMouseMove = function (moveEvt) {
+          moveEvt.preventDefault();
 
-        var onMouseMove = function(moveEvt) {
-            moveEvt.preventDefault();
+          var shift = {
+              x: startCoords.x - moveEvt.clientX,
+              y: startCoords.y - moveEvt.clientY
+          };
 
-            var shift = {
-                x: startCoords.x - moveEvt.clientX,
-                y: startCoords.y - moveEvt.clientY
-            };
+          startCoords = {
+              x: moveEvt.clientX,
+              y: moveEvt.clientY
+          };
 
-            startCoords = {
-                x: moveEvt.clientX,
-                y: moveEvt.clientY
-            };
+          var mainPinTop = mainPin.offsetTop - shift.y;
+          var mainPinLeft = mainPin.offsetLeft - shift.x;
 
-            var mainPinTop = mainPin.offsetTop - shift.y;
-            var mainPinLeft = mainPin.offsetLeft - shift.x;
+          if (mainPinTop < mapTopLimit) {
+              mainPinTop = mapTopLimit;
+          } else if (mainPinTop > mapBottomLimit) {
+              mainPinTop = mapBottomLimit;
+          }
 
-            if (mainPinTop < mapTopLimit) {
-                mainPinTop = mapTopLimit;
-            } else if (mainPinTop > mapBottomLimit) {
-                mainPinTop = mapBottomLimit;
-            }
+          if (mainPinLeft < MAP_LEFT_LIMIT) {
+              mainPinLeft = MAP_LEFT_LIMIT;
+          } else if (mainPinLeft > mapRightLimit) {
+              mainPinLeft = mapRightLimit;
+          }
 
-            if (mainPinLeft < MAP_LEFT_LIMIT) {
-                mainPinLeft = MAP_LEFT_LIMIT;
-            } else if (mainPinLeft > mapRightLimit) {
-                mainPinLeft = mapRightLimit;
-            }
+          mainPin.style.top = mainPinTop + 'px';
+          mainPin.style.left = mainPinLeft + 'px';
 
-            mainPin.style.top = mainPinTop + 'px';
-            mainPin.style.left = mainPinLeft + 'px';
+          var mainPinPosition = (mainPinLeft + MAIN_PIN_WIDTH / 2) + ', ' + (mainPinTop + MAIN_PIN_HEIGHT);
+          window.form.addressForm.value = mainPinPosition;
+      };
 
-            var mainPinPosition = (mainPinLeft + MAIN_PIN_WIDTH / 2) + ', ' + (mainPinTop + MAIN_PIN_HEIGHT);
-            window.form.addressForm.value = mainPinPosition;
-        };
+      var onMouseUp = function (upEvt) {
+          upEvt.preventDefault();
 
-        var onMouseUp = function(upEvt) {
-            upEvt.preventDefault();
+          document.removeEventListener('mousemove', onMouseMove);
+          document.removeEventListener('mouseup', onMouseUp);
 
-            document.removeEventListener('mousemove', onMouseMove);
-            document.removeEventListener('mouseup', onMouseUp);
+          if (dragged) {
+              var onClickPreventDefault = function (onClickEvt) {
+                  onClickEvt.preventDefault();
+                  mainPin.removeEventListener('click', onClickPreventDefault);
+              };
+              mainPin.addEventListener('click', onClickPreventDefault);
+          }
+      };
 
-            if (dragged) {
-                var onClickPreventDefault = function(onClickEvt) {
-                    onClickEvt.preventDefault();
-                    mainPin.removeEventListener('click', onClickPreventDefault);
-                };
-                mainPin.addEventListener('click', onClickPreventDefault);
-            }
-        };
+      document.addEventListener('mousemove', onMouseMove);
+      document.addEventListener('mouseup', onMouseUp);
+  });
 
-        document.addEventListener('mousemove', onMouseMove);
-        document.addEventListener('mouseup', onMouseUp);
-    });
-
-    window.map = {
-        map: map,
-        pinElemList: pinElemList,
-        pinList: pinList,
-        mainPin: mainPin,
-        mainPinLocationY: MAIN_PIN_LOCATION_Y,
-        mainPinLocationX: MAIN_PIN_LOCATION_X,
-        mainPinPositionFirst: mainPinPositionFirst
-    };
+  window.map = {
+      map: map,
+      pinElemList: pinElemList,
+      pinList: pinList,
+      mainPin: mainPin,
+      mainPinLocationY: MAIN_PIN_LOCATION_Y,
+      mainPinLocationX: MAIN_PIN_LOCATION_X,
+      mainPinPositionFirst: mainPinPositionFirst
+  };
 
 })();
--- js/util.js
+++ js/util.js
@@ -6,5 +6,5 @@
     getRandomValue: function (min, max) {
       return Math.floor(Math.random() * (max - min + 1)) + min;
     }
-  }
+  };
 })();
